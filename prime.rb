ad5b435324b0394b08e2c181f33a3d8810aa8ee4
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 04:58:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(integer)
-  n=2
-  while n < integer
-    return false if integer % n == 0
-    n += 1
-  end
-  true
+ (2..(integer - 1)).each do |n|
+   return false if integer % n == 0
+ end
+ true
 end
